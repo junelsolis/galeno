2381b1c8466a62c318205d7a9f059901f1c3f441
Junel Solis <junel.solis@gmail.com>
2019-01-07 13:19:19 +0500

    write user feature test

--- app/app/User.php
+++ app/app/User.php
@@ -10,20 +10,13 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
+
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
--- app/tests/Feature/UserTest.php
+++ app/tests/Feature/UserTest.php
@@ -5,16 +5,43 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+
+  use RefreshDatabase;
+
+  private $user;
+
+  public function setUp() {
+    parent::setUp();
+
+    $this->user = factory('App\User')->create([
+      'id' => 33
+    ]);
+  }
+
+  /** @test */
+  public function a_user_has_roles() {
+
+    DB::table('user_roles')->insert([
+      'user_id' => '33',
+      'role' => 'physician'
+    ]);
+
+    $this->assertTrue(count($this->user->roles()) == 1);
+
+    DB::table('user_roles')->insert([
+      'user_id' => '33',
+      'role' => 'clinician'
+    ]);
+
+    $this->assertTrue(count($this->user->roles()) == 2);
+
+  }
+
+  /** @test */
+  public function a_user_can_be_disabled() {}
+
 }
