eddec86489a628828451f0902f0ecade0a4bae74
Junel Solis <junel.solis@gmail.com>
2019-01-23 18:07:49 +0500

    add Medication model, factory and migration

---
+++ app/database/factories/MedicationFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\User;
+use App\Patient;
+use App\Diagnosis;
+
+$factory->define(App\Medication::class, function (Faker $faker) {
+
+    $meds = [
+      'Vicodin',
+      'Synthroid',
+      'Levoxyl',
+      'Amoxil',
+      'Glucophage',
+      'Zestril',
+      'Neurontin',
+      'Delason',
+      'Lipitor',
+      'Norvasc',
+      'Zithromax',
+      'hydrochlorothiazide',
+      'Nexium',
+      'Singulair',
+      'Crestol',
+    ];
+
+    return [
+        'patient_id' => factory('App\Patient')->create()->id,
+        'provider_id' => factory('App\User')->create()->id,
+        'diagnosis_id' => factdory('App\Diagnosis')->create()->id,
+        'name' => $meds[array_rand($meds)],
+        
+    ];
+});
---
+++ app/database/migrations/2019_01_23_124931_create_medications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('patient_id');
+            $table->unsignedInteger('provider_id');
+            $table->unsignedInteger('diagnosis_id');
+            $table->string('name');
+            $table->string('dosage')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medications');
+    }
+}
