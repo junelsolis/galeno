8efcb11f30f73087b03137359de973c2283662b8
Junel Solis <junel.solis@gmail.com>
2019-01-25 14:54:26 +0500

    update medication factory to include encounter id

--- app/database/factories/MedicationFactory.php
+++ app/database/factories/MedicationFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 use App\User;
 use App\Patient;
 use App\Diagnosis;
+use App\Encounter;
 
 $factory->define(App\Medication::class, function (Faker $faker) {
 
@@ -29,6 +30,7 @@ $factory->define(App\Medication::class, function (Faker $faker) {
         'patient_id' => factory('App\Patient')->create()->id,
         'provider_id' => factory('App\User')->create()->id,
         'diagnosis_id' => factory('App\Diagnosis')->create()->id,
+        'encounter_id' => factory('App\Encounter')->create()->id,
         'name' => $meds[array_rand($meds)],
         'active' => true
     ];
