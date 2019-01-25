bbdf44a3e046cfb3e90dc096fd37d5faa699d3b0
Junel Solis <junel.solis@gmail.com>
2019-01-25 17:58:23 +0500

    create login screen, plus some site-wide scss defaults.

--- app/resources/views/login.blade.php
+++ app/resources/views/login.blade.php
@@ -2,9 +2,29 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <link rel='stylesheet' href='/css/app.css' />
+    <script src="/js/app.js"></script>
+    <title>MedicOffice</title>
   </head>
   <body>
-    login view
+    <div class='login'>
+      <div class='box'>
+        <div class='image text-center'>
+          <img src='images/medicoffice-logo@2x.png' />
+        </div>
+        <form method='post' action='/'>
+          {{ csrf_field() }}
+          <input type='text' name='username' required placeholder='Username or e-mail address' />
+
+          <input type='password' name='password' required placeholder='Password' />
+
+          <button type='submit' class='primary button expanded'>Login</button>
+        </form>
+      </div>
+    </div>
+
+    <script>
+      $(document).foundation();
+    </script>
   </body>
 </html>
