a12e8fe9f628286ec323e78288f4d2b10b1674ae
Junel Solis <junel.solis@gmail.com>
2019-01-25 13:18:05 +0500

    add telescope as a dev dependency

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
     }
 
     /**
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+      // if ($this->app->isLocal()) {
+      //   $this->app->register(TelescopeServiceProvider::class);
+      // }
     }
 }
