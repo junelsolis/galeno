85bc905235522642bf4898b3ac7a276df8d88bd3
Junel Solis <junel.solis@gmail.com>
2019-01-27 11:27:00 +0500

    set viewport

--- app/resources/views/login.blade.php
+++ app/resources/views/login.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel='stylesheet' href='/css/app.css' />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <script src="/js/app.js"></script>
