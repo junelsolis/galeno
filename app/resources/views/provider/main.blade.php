942071bc9e2361fb4dede30e831a01fecb891185
Junel Solis <junel.solis@gmail.com>
2019-01-27 11:26:52 +0500

    some work on provider menu

--- app/resources/views/provider/main.blade.php
+++ app/resources/views/provider/main.blade.php
@@ -2,9 +2,49 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='/css/app.css' />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <script src="/js/app.js"></script>
+    <title>Medicoffice | Provider</title>
   </head>
-  <body>
-    provider main
+  <body class='provider'>
+    <div class='grid-x'>
+      <div class='menu cell medium-3 large-2'>
+        <div class='logo'>
+          <img src='images/medicoffice-logo.png' />
+        </div>
+        <div class='items'>
+          <div class='menu-section'>
+            <ul>
+              <li class='header'><i class="fas fa-user"></i>&nbsp;Patients</li>
+                <li class='item'><a href=''>Patient Index</a></li>
+                <li class='item'><a href=''>Assessments</a></li>
+                <li class='item'><a href=''>Encounters</a></li>
+            </ul>
+          </div>
+          <div class='menu-section'>
+            <ul>
+              <li class='header'><i class="fas fa-pills"></i>&nbsp;Medication</li>
+                <li class='item'><a href=''>Medication Index</a></li>
+                <li class='item'><a href=''>Assessments</a></li>
+                <li class='item'><a href=''>Encounters</a></li>
+            </ul>
+          </div>
+          <div class='menu-section'>
+            <ul>
+              <li class='header'><i class="fas fa-flask"></i>&nbsp;Laboratory</li>
+                <li class='item'><a href=''>Results</a></li>
+            </ul>
+          </div>
+          <div class='menu-section'>
+            <ul>
+              <li class='header'><i class="fas fa-radiation"></i>&nbsp;Imaging</li>
+                <li class='item'><a href=''>Results</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
