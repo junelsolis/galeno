69d15976d94890df57b63ab76573e71a3cd5a2e1
Junel Solis <junel.solis@gmail.com>
2019-01-27 17:37:46 +0500

    work on provider dashboard

--- app/resources/views/provider/main.blade.php
+++ app/resources/views/provider/main.blade.php
@@ -12,52 +12,43 @@
     <div class='grid-x'>
       @include('provider.menu')
 
-      <div class='chart cell medium-6 large-8'>
-        <div class='fake-space'>
-
-        </div>
-        <ul class="tabs" data-tabs id="example-tabs" data-auto-focus="false">
-          <li class="tabs-title is-active"><a href="#panel1">Patient Summary</a></li>
-          <li class="tabs-title"><a data-tabs-target="panel2" href="#panel2">Vital Signs</a></li>
-          <li class="tabs-title"><a data-tabs-target="panel3" href="#panel3">Encounters & Notes</a></li>
-          <li class="tabs-title"><a data-tabs-target="panel4" href="#panel4">Diagnoses & Medications</a></li>
-          <li class="tabs-title"><a data-tabs-target="panel5" href="#panel5">Laboratory & Imaging</a></li>
-        </ul>
-
-
-        <div class="tabs-content" data-tabs-content="example-tabs" data-auto-focus="false">
-          <div class="tabs-panel is-active" id="panel1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <div class='section'>
-              <div class='header'>
-                <span>Problems</span>
-              </div>
+      <div class='cell medium-6 large-8'>
+        <div class='dashboard'>
+          <div class='widget'>
+            <div class='header'>
+              <span>Patients Today</span>
             </div>
+            <div class='content'>
 
-            <div class='section'>
-              <div class='header'>
-                <span>Allergies</span>
-              </div>
-            </div>
-
-            <div class='section'>
-              <div class='header'>
-                <span>Medications</span>
-              </div>
             </div>
           </div>
-          <div class="tabs-panel" id="panel2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          <div class='widget'>
+            <div class='header'>
+              <span>Unsigned Encounters</span>
+            </div>
+            <div class='content'>
+
+            </div>
           </div>
-          <div class="tabs-panel" id="panel3">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          <div class='widget'>
+            <div class='header'>
+              <span>Laboratory Results</span>
+            </div>
+            <div class='content'>
+
+            </div>
           </div>
-          <div class="tabs-panel" id="panel4">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="tabs-panel" id="panel5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+          <div class='widget'>
+            <div class='header'>
+              <span>Imaging Results</span>
+            </div>
+            <div class='content'>
+
+            </div>
           </div>
         </div>
       </div>
