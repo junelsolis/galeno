68f2cac8b10c34523e63216ad25a42dbdf0c312a
Junel Solis <junel.solis@gmail.com>
2019-01-27 11:31:49 +0500

    place provider menu in its own blade file and include where needed.

--- app/resources/views/provider/main.blade.php
+++ app/resources/views/provider/main.blade.php
@@ -10,41 +10,7 @@
   </head>
   <body class='provider'>
     <div class='grid-x'>
-      <div class='menu cell medium-3 large-2'>
-        <div class='logo'>
-          <img src='images/medicoffice-logo.png' />
-        </div>
-        <div class='items'>
-          <div class='menu-section'>
-            <ul>
-              <li class='header'><i class="fas fa-user"></i>&nbsp;Patients</li>
-                <li class='item'><a href=''>Patient Index</a></li>
-                <li class='item'><a href=''>Assessments</a></li>
-                <li class='item'><a href=''>Encounters</a></li>
-            </ul>
-          </div>
-          <div class='menu-section'>
-            <ul>
-              <li class='header'><i class="fas fa-pills"></i>&nbsp;Medication</li>
-                <li class='item'><a href=''>Medication Index</a></li>
-                <li class='item'><a href=''>Assessments</a></li>
-                <li class='item'><a href=''>Encounters</a></li>
-            </ul>
-          </div>
-          <div class='menu-section'>
-            <ul>
-              <li class='header'><i class="fas fa-flask"></i>&nbsp;Laboratory</li>
-                <li class='item'><a href=''>Results</a></li>
-            </ul>
-          </div>
-          <div class='menu-section'>
-            <ul>
-              <li class='header'><i class="fas fa-radiation"></i>&nbsp;Imaging</li>
-                <li class='item'><a href=''>Results</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      @include('provider.menu')
     </div>
   </body>
 </html>
