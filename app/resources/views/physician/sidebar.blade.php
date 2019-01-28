08b94f5b99d2437c4b9ffe96e414407649c98b4b
Junel Solis <junel.solis@gmail.com>
2019-01-28 19:55:42 +0500

    more work on physician dashboard

--- app/resources/views/physician/sidebar.blade.php
+++ app/resources/views/physician/sidebar.blade.php
@@ -4,13 +4,27 @@
 
       <div class='top'>
         <p><strong>Junel Solis</strong> | Physician</p>
-        <i id='bars' class="fas fa-bars"></i>
+        <a><i id='bars' class="fas fa-bars"></i></a>
       </div>
 
 
       <div class='items'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>    
+        <a class='hollow primary small expanded button'><i class="fas fa-plus"></i> Patient</a>
+        <a class='hollow primary small expanded button'><i class="fas fa-plus"></i> Encounter</a>
+        <a class='hollow primary small expanded button'><i class="fas fa-plus"></i> Lab Result</a>
+        <a class='hollow primary small expanded button'><i class="fas fa-plus"></i> Imaging Result</a>
+
+
+        <hr />
+        Good morning, Junel!
+        <br /><br />
+        You have <strong>28</strong> patients waiting. There are <strong>15</strong> unsigned <a href=''></a>encounters.
+        <hr />
+        <p style='color:grey; font-size:0.95rem;'>
+          This is free software developed by <a href='https://www.junelsolis.com'>Junel Solis MD</a>.<br /><br />
+          For technical support, contact me <a href='https://www.junelsolis.com'>directly</a> or on the <a href='https://www.github.com/junelsolis/medicoffice'>Github</a> repository page.
+        </p>
+      </div>
     </div>
   </div>
 
