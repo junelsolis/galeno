7a01b4a0b78f6270cb2f9c243e0ee2693432e2f4
Junel Solis <junel.solis@gmail.com>
2019-01-29 13:23:23 +0500

    include some random chief complaints

--- app/database/factories/EncounterFactory.php
+++ app/database/factories/EncounterFactory.php
@@ -6,11 +6,31 @@ use App\User;
 
 
 $factory->define(App\Encounter::class, function (Faker $faker) {
+
+    $chief_complaints = [
+      'chest pain',
+      'shortness of breath',
+      'abdominal pain',
+      'back pain',
+      'flank pain',
+      'headache',
+      'joint pain',
+      'coughing',
+      'runny nose',
+      'sore throat',
+      'diarrhea',
+      'constipation',
+      'painful urination',
+      'fatigue',
+      'headaches',
+      'ear pain'
+    ];
+
     return [
       'patient_id' => factory('App\Patient')->create()->id,
       'provider_id' => factory('App\User')->create()->id,
       'date' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now', $timezone = null),
-      'chief_complaint' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+      'chief_complaint' => $chief_complaints[array_rand($chief_complaints)],
       'reminder' => $faker->sentence($nbWords = 6, $variableNbWords = true),
     ];
 });
