bcac35eda969d44472bc3f1f2df60890ab474209
Junel Solis <junel.solis@gmail.com>
2019-01-29 10:29:16 +0500

    change patient seeder to create today's encounters and patients.

--- app/database/seeds/PatientSeeder.php
+++ app/database/seeds/PatientSeeder.php
@@ -2,13 +2,39 @@
 
 use Illuminate\Database\Seeder;
 use App\Patient;
+use App\Encounter;
+use Carbon\Carbon;
 
 class PatientSeeder extends Seeder
 {
 
 
+
     public function run()
     {
+
+        // create 60 patients for John Smith, physician
+        $count = 0;
+
+        while ($count < 59) {
+          $patient = factory('App\Patient')->create([
+            // 'sid' => $count + 1,
+            'provider_id' => 2
+          ]);
+
+
+          $random = rand(1,4);
+          if ($random == 4) {
+            factory('App\Encounter')->create([
+              'patient_id' => $patient->id,
+              'provider_id' => 2,
+              'date' => Carbon::now()
+            ]);
+          }
+
+          $count++;
+        }
+
         // create 5 physicians with 15 random patients each
         $count = 0;
 
@@ -21,9 +47,26 @@ class PatientSeeder extends Seeder
             'role' => 'physician'
           ]);
 
-          factory('App\Patient',15)->create([
-            'provider_id' => $doctor->id
-          ]);
+          $int = 70;
+          while ($int < 84) {
+            $patient = factory('App\Patient')->create([
+              'id' => $int * 10 * $count,
+              'provider_id' => $doctor->id
+            ]);
+
+            // randomly create an encounter
+            $random = rand(1,5);
+            if ($random == 5) {
+              factory('App\Encounter')->create([
+                'patient_id' => $patient->id,
+                'provider_id' => $doctor->id,
+                'date' => Carbon::now()
+              ]);
+            }
+
+            $int++;
+          }
+
 
           $count++;
         }
