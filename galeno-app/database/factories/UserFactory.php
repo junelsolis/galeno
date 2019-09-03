00732436d0d637c34e62d6d3027deca65704a7c7
Junel Solis <junel.solis@gmail.com>
2019-09-03 22:15:09 +0300

    customize user factory

--- galeno-app/database/factories/UserFactory.php
+++ galeno-app/database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,8 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'phone' => $faker->phoneNumber,
+        'password' => Hash::make(Str::random(17)),
         'remember_token' => Str::random(10),
     ];
 });
