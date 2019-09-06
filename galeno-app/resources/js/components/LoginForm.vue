42f6757ce69aa983d62bf5e8f83777adb224938e
Junel Solis <junel.solis@gmail.com>
2019-09-07 00:07:19 +0300

    create a login vue component

---
+++ galeno-app/resources/js/components/LoginForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='login-form'>
+
+    <div class='input-group'>
+      <input type='text' v-model='email' placeholder='Email address' />
+      <input type='password' v-model='password' placeholder='Password' />
+    </div>
+
+    <div class='mt-4 text-center'>
+      <button class='standard-button'>Login</button>
+    </div>
+
+
+  </div>
+
+</template>
+
+<style lang='scss'>
+
+  .login-form {
+
+    
+
+
+
+  }
+
+
+</style>
+
+
+<script>
+
+    export default {
+
+
+
+    }
+
+</script>
