51c4f6881893a575d660c41fdf826e944995cefb
Junel Solis <junel@junelsolis.com>
2019-09-14 22:18:31 +0300

    Apply fixes from StyleCI (#8)

--- galeno-app/tests/TestCase.php
+++ galeno-app/tests/TestCase.php
@@ -8,43 +8,40 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-
     public function setUp(): void
     {
-      parent::setUp();
+        parent::setUp();
 
-      \DB::table('roles')->insert([
+        \DB::table('roles')->insert([
         ['name' => 'physician', 'created_at' => now(), 'updated_at' => now()],
         ['name' => 'nurse', 'created_at' => now(), 'updated_at' => now()],
         ['name' => 'staff', 'created_at' => now(), 'updated_at' => now()],
         ['name' => 'admin', 'created_at' => now(), 'updated_at' => now()],
       ]);
-
     }
-
 
     public function user()
     {
-      return factory('App\User')->create();
+        return factory('App\User')->create();
     }
 
     public function staff()
     {
-      return factory('App\User')->create()->assignRoles('staff');
+        return factory('App\User')->create()->assignRoles('staff');
     }
 
     public function admin()
     {
-      return factory('App\User')->create()->assignRoles('admin');
+        return factory('App\User')->create()->assignRoles('admin');
     }
 
     public function nurse()
     {
-      return factory('App\User')->create()->assignRoles('nurse');
+        return factory('App\User')->create()->assignRoles('nurse');
     }
 
     public function physician()
     {
-      return factory('App\User')->create()->assignRoles('physician');
+        return factory('App\User')->create()->assignRoles('physician');
     }
 }
