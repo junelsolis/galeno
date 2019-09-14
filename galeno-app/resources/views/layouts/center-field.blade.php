b9065c67e9add6669274a5afd8254f3cf6753aec
Junel Solis <junel.solis@gmail.com>
2019-09-15 00:56:44 +0300

    more frontend code

--- galeno-app/resources/views/layouts/center-field.blade.php
+++ galeno-app/resources/views/layouts/center-field.blade.php
@@ -6,16 +6,16 @@
     <title>Galeno &mdash; @yield('title')</title>
   </head>
   <body class='bg-dark'>
-    <div id='app' class='min-h-screen bg-grey-100 flex flex-col items-center justify-center'>
+    <div id='app' class='min-h-screen flex flex-col items-center justify-center'>
       <div class='container mx-auto flex-grow flex items-center justify-center'>
         @yield('content')
       </div>
 
-      <footer class='bg-grey-200 py-2 px-3 w-full'>
+      <footer class='bg-accent py-4 px-3 w-full'>
         <div class='flex items-center justify-center'>
           <p class='text-xs text-center'>
-            Galeno &mdash; Simple Patient Charts<br />
-            <a class='text-blue-100' href='https://www.github.com/junelsolis.galeno' target='_blank'>github page</a>
+            Galeno &mdash; Simple Patient Records<br />
+            <a class='text-light' href='https://www.github.com/junelsolis.galeno' target='_blank'>github page</a>
           </p>
         </div>
       </footer>
--- galeno-app/resources/views/layouts/parent.blade.php
+++ galeno-app/resources/views/layouts/parent.blade.php
@@ -6,12 +6,13 @@
   </head>
   <body class='' style='overscroll-behavior: none;'>
     <div id='app' class='w-full h-screen bg-grey-100 flex'>
+
       <div class='bg-white py-8 flex flex-col' style='width:4.7rem;box-shadow: 3px 0px 10px rgba(0,0,0,0.08);'>
         <div class='bg-accent rounded-full mx-auto' style='width:3rem;height: 3rem;overflow:hidden'>
           <img src='https://www.gravatar.com/avatar/d9a9e7f53d61e12ef933d846da67ff7a' class='h-full w-auto'/>
         </div>
         <div class='w-full flex-grow'>
-          
+
         </div>
         <div class='flex items-center justify-center'>
           <p class='text-blue-100 text-lg'>
@@ -19,8 +20,11 @@
           </p>
         </div>
       </div>
+
+
+
       <div class='flex-grow flex h-full'>
-        <div class='w-3/4 p-10'>
+        <div class='w-3/4 py-10 px-16'>
           <div class='w-full flex items-center justify-between'>
             <div class='flex items-center text-grey-400'>
               <p class='font-bold text-2xl mr-40 pb-4'>
@@ -43,6 +47,62 @@
               <p class='ml-8 text-lg text-grey-300'>
                 <i class="far fa-bell"></i>
               </p>
+            </div>
+          </div>
+
+
+          <div class='w-full flex items-center mt-12' style='height:5rem;'>
+            <div class='border-l-2 border-grey-200 w-full h-full px-6 flex justify-center flex-col'>
+              <p class='text-grey-400 text-sm'>
+                Queued
+              </p>
+              <div class='flex items-center'>
+                <p class='text-2xl'>
+                  18
+                </p>
+                <p class='ml-3 text-xl'>
+                  Patients
+                </p>
+              </div>
+            </div>
+            <div class='border-l-2 border-grey-200 w-full h-full px-6 flex justify-center flex-col'>
+              <p class='text-grey-400 text-sm'>
+                All Patients
+              </p>
+              <div class='flex items-center'>
+                <p class='text-2xl'>
+                  350
+                </p>
+                <p class='ml-3 text-xl'>
+                  Patients
+                </p>
+              </div>
+            </div>
+            <div class='border-l-2 border-grey-200 w-full h-full px-6 flex justify-center flex-col'>
+              <p class='text-grey-400 text-sm'>
+                All Doctors
+              </p>
+              <div class='flex items-center'>
+                <p class='text-2xl'>
+                  2
+                </p>
+                <p class='ml-3 text-xl'>
+                  Doctors
+                </p>
+              </div>
+            </div>
+            <div class='border-l-2 border-grey-200 w-full h-full px-6 flex justify-center flex-col'>
+              <p class='text-grey-400 text-sm'>
+                All Documents
+              </p>
+              <div class='flex items-center'>
+                <p class='text-2xl'>
+                  427
+                </p>
+                <p class='ml-3 text-xl'>
+                  Documents
+                </p>
+              </div>
             </div>
           </div>
         </div>
